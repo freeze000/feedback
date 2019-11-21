263fece3bfa3dd487ae62f0b7baef826f88cdde6
freeze000 <freeze000@mail.ru>
2019-11-21 14:04:32 +0000

    crossdomain auth;

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,10 @@
 <?php
 return [
+    // cookie common key (frontend + backend)
+	// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+    'cookieValidationKey' => '',
+
+    // cookie domain 
+    'cookieDomain' => '.y2aaa.test',
 ];
+
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,6 +14,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'cookieValidationKey' => $params['cookieValidationKey'],
         ],
         'user' => [
             'identityClass' => 'common\models\User',
